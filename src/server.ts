962bd05a388a948056dcb02baa5896c600e623dc
Hoyeong Yoo <crystal23733@gmail.com>
2024-07-03 16:58:57 +0900

    :package:서버에서 정적파일 읽어내는 경로 할당

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,18 @@
 import express from 'express';
+import path from 'path';
+
+const app = express();
 
 const PORT: number = 8080;
+
+// * static 사용을 위한 경로 변수 할당
+const publicPath: string = path.join(__dirname, 'public');
+const srcPath: string = path.join(__dirname, 'src');
+
+// * 정적 파일 읽는 미들웨어
+app.use(express.static(publicPath));
+app.use(express.static(srcPath));
+
+app.listen(PORT, () => {
+  console.log(`http://localhost:${PORT}`);
+});
